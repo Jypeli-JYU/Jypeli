cf3c49b2a859524d49b38dd870fbb86c05b4f88f
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-05-29 23:38:50 +0000

    WinRT Pong (tested on tablet)

--- branches/MonoGame/Jypeli/Physics/PhysicsInterface.cs
+++ branches/MonoGame/Jypeli/Physics/PhysicsInterface.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Reflection;
+using System.IO;
+
+#if WINRT
+using System.Threading.Tasks;
+using Windows.Storage;
+#endif
 
 namespace Jypeli.Physics
 {
@@ -10,14 +16,51 @@ namespace Jypeli.Physics
     {
         public static IPhysicsClient GetClient()
         {
-            foreach ( var assembly in AppDomain.CurrentDomain.GetAssemblies() )
+            foreach ( var assembly in GetAssemblies() )
             {
-                Type clientClass = assembly.GetType( "Jypeli.Physics.PhysicsClient", false );
+                Type clientClass = assembly.GetType( "Jypeli.Physics.PhysicsClient" );
                 if ( clientClass != null )
-                    return (IPhysicsClient)Activator.CreateInstance( clientClass );
+                     return (IPhysicsClient)Activator.CreateInstance( clientClass );
             }
 
             return null;
+        }
+
+        private static IEnumerable<Assembly> GetAssemblies()
+        {
+#if WINRT
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var assemblies = new List<Assembly>();
+
+            IEnumerable<StorageFile> files = null;
+
+            var operation = folder.GetFilesAsync();
+            operation.Completed = async ( r, s ) =>
+            {
+                var result = await r;
+                files = result;
+            };
+
+            while ( files == null ) ;
+
+            foreach ( var file in files )
+            {
+                if ( file.FileType == ".dll" )
+                {
+                    var name = new AssemblyName() { Name = System.IO.Path.GetFileNameWithoutExtension( file.Name ) };
+                    try
+                    {
+                        Assembly asm = Assembly.Load( name );
+                        assemblies.Add( asm );
+                    }
+                    catch { }
+                }
+            }
+
+            return assemblies;
+#else
+            return AppDomain.CurrentDomain.GetAssemblies();
+#endif
         }
     }
 }

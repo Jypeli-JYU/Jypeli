b181ee027ad86dd79d49d7b88f2010aa79860fd4
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-07-26 10:57:19 +0000

    Beginning of F# SimplePhysics

---
+++ SimplePhysics/SimplePhysics-FS2010/PhysicsGame.fs
@@ -0,0 +1,58 @@
+﻿module PhysicsGame
+
+open Jypeli
+open SimplePhysics
+
+type public PhysicsGame() =
+    inherit Game()
+
+    let mutable _gravity = Vector.Zero
+    let mutable objects : PhysicsObject list = []
+    let maxdt = 0.01
+
+    member this.Gravity
+        with get() = _gravity
+        and set(a) = _gravity <- a
+
+    override this.Add(obj, layer) =
+        match obj with
+            | :? PhysicsObject as physObj ->
+                objects <- physObj :: objects
+                base.Add(obj, layer)
+            | _ -> base.Add(obj, layer)
+
+        base.Add(obj, layer)
+
+    override this.Update(t : Time) =
+        let mutable dt = t.SinceLastUpdate.TotalSeconds
+        while (dt < maxdt) do
+            this.Integrate(maxdt)
+            dt <- dt - maxdt
+        this.Integrate(dt);
+        this.Cleanup();
+        base.Update(t)
+
+    member this.Integrate(dt) =
+        match dt with
+            | 0.0 -> ()
+            | _ ->
+                List.map (fun o -> this.UpdateBody(dt, o.Body) objects) |> ignore
+                this.SolveCollisions()
+
+(*
+PhysicsBody body = (PhysicsBody)physObjects[i].Body;
+if ( !body.IgnoresGravity && !body.IgnoresPhysicsLogics )
+    body.Velocity += Gravity * body.MassInv * dt;
+body.Velocity += body.Acceleration * dt;
+body.Position += body.Velocity * dt;
+*)
+
+    member this.UpdateBody(dt, b : PhysicsBody) =
+        b.Velocity <- b.Velocity + b.Acceleration * dt
+
+    member this.Cleanup() =
+        ()
+
+    member this.SolveCollisions() =
+        ()
+

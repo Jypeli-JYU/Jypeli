6a8eceed7e1593c64eccc7f136f679a7e0cafb8f
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-09-07 12:16:20 +0000

    Tile maps and text file reading, platform game test project. Text files still need to be precompiled to xnb format though.

---
+++ branches/MonoGame/Jypeli/Levels/ColorTileMap.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jypeli
+{
+    /// <summary>
+    /// Ruutukartta, jonka avulla olioita voidaan helposti asettaa tasavälein ruudukkoon.
+    /// Ruutukartta koostuu kirjoitusmerkeistä (<c>char</c>), joihin voi liittää
+    /// aliohjelman, joka luo merkkiä vastaavan olion.
+    /// </summary>
+    public class ColorTileMap : AbstractTileMap<Color>
+    {
+        private double _tolerance = 30;
+
+        protected override Color Null
+        {
+            get { return Color.Transparent; }
+        }
+
+        /// <summary>
+        /// Väritoleranssi. Mitä pienempi toleranssi, sitä tarkemmin eri värit erotellaan toisistaan.
+        /// Nollatoleranssilla värit on annettava tarkkoina rgb-koodeina, suuremmilla toleransseilla
+        /// riittää "sinne päin".
+        /// </summary>
+        public double ColorTolerance
+        {
+            get { return _tolerance; }
+            set
+            {
+                if (value < 0) throw new ArgumentException("Tolerance must not be negative.");
+                _tolerance = value;
+            }
+        }
+        
+        /// <summary>
+        /// Luo uuden ruutukartan.
+        /// </summary>
+        /// <param name="img">Kuva, jossa jokainen pikseli vastaa oliota.</param>
+        public ColorTileMap( Image img )
+            : base( img.GetData() )
+        {
+        }
+
+        /// <summary>
+        /// Luo uuden ruutukartan.
+        /// </summary>
+        /// <param name="assetName">Kuvatiedoston nimi.</param>
+        public ColorTileMap(string assetName)
+            : this(Game.LoadImage(assetName))
+        {
+        }
+
+        /// <summary>
+        /// Lukee ruutukentän Content-projektin kuvatiedostosta.
+        /// </summary>
+        /// <param name="assetName">Tiedoston nimi</param>        
+        public static ColorTileMap FromLevelAsset( string assetName )
+        {
+            return new ColorTileMap( Game.LoadImage( assetName ) );
+        }
+
+        protected virtual bool ItemEquals( Color a, Color b )
+        {
+            return ( a.AlphaComponent == b.AlphaComponent && Color.Distance( a, b ) <= ColorTolerance );
+        }
+
+        /// <summary>
+        /// Määrittää, että tietyn ruutukentän symbolin (<c>tileSymbol</c>) kohdalla
+        /// kutsutaan aliohjelmaa <c>f</c>. Huom! Käytä tämän aliohjelman kanssa metodia
+        /// Execute.
+        /// </summary>
+        /// <param name="hexCode">Heksakoodi värille</param>
+        /// <param name="method">Aliohjelma</param>
+        public void SetTileMethod(string hexCode, TileMethod method)
+        {
+            SetTileMethod(Color.FromHexCode(hexCode), method);
+        }
+
+        /// <summary>
+        /// Määrittää, että tietyn ruutukentän symbolin (<c>tileSymbol</c>) kohdalla
+        /// kutsutaan aliohjelmaa <c>f</c>. Huom! Käytä tämän aliohjelman kanssa metodia
+        /// Execute.
+        /// </summary>
+        /// <param name="hexCode">Heksakoodi värille</param>
+        /// <param name="method">Aliohjelma</param>
+        /// <param name="p1">Parametri</param>
+        public void SetTileMethod<T1>(string hexCode, TileMethod<T1> method, T1 p1)
+        {
+            SetTileMethod(Color.FromHexCode(hexCode), method, p1);
+        }
+
+        /// <summary>
+        /// Määrittää, että tietyn ruutukentän symbolin (<c>tileSymbol</c>) kohdalla
+        /// kutsutaan aliohjelmaa <c>f</c>. Huom! Käytä tämän aliohjelman kanssa metodia
+        /// Execute.
+        /// </summary>
+        /// <param name="hexCode">Heksakoodi värille</param>
+        /// <param name="method">Aliohjelma</param>
+        /// <param name="p1">Parametri</param>
+        /// <param name="p2">Parametri</param>
+        public void SetTileMethod<T1, T2>(string hexCode, TileMethod<T1, T2> method, T1 p1, T2 p2)
+        {
+            SetTileMethod(Color.FromHexCode(hexCode), method, p1, p2);
+        }
+
+        /// <summary>
+        /// Määrittää, että tietyn ruutukentän symbolin (<c>tileSymbol</c>) kohdalla
+        /// kutsutaan aliohjelmaa <c>f</c>. Huom! Käytä tämän aliohjelman kanssa metodia
+        /// Execute.
+        /// </summary>
+        /// <param name="hexCode">Heksakoodi värille</param>
+        /// <param name="method">Aliohjelma</param>
+        /// <param name="p1">Parametri</param>
+        /// <param name="p2">Parametri</param>
+        /// <param name="p3">Parametri</param>
+        public void SetTileMethod<T1, T2, T3>(string hexCode, TileMethod<T1, T2, T3> method, T1 p1, T2 p2, T3 p3)
+        {
+            SetTileMethod(Color.FromHexCode(hexCode), method, p1, p2, p3);
+        }
+    }
+}
---
+++ branches/MonoGame/Jypeli/Levels/TileMap.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Jypeli
+{
+    /// <summary>
+    /// Ruutukartta, jonka avulla olioita voidaan helposti asettaa tasavälein ruudukkoon.
+    /// Ruutukartta koostuu kirjoitusmerkeistä (<c>char</c>), joihin voi liittää
+    /// aliohjelman, joka luo merkkiä vastaavan olion.
+    /// </summary>
+    public class TileMap : AbstractTileMap<char>
+    {
+        private Dictionary<char, Func<GameObject>> oldLegend = new Dictionary<char, Func<GameObject>>();
+
+        protected override char Null
+        {
+            get { return ' '; }
+        }
+
+        /// <summary>
+        /// Asettaa merkin vastaamaan aliohjelmaa, joka luo olion. Huom! Käytä tämän
+        /// syntaksin kanssa metodia <c>Insert</c>.
+        /// </summary>
+        public Func<GameObject> this[char c]
+        {
+            get { return oldLegend[c]; }
+            set { oldLegend[c] = value; }
+        }
+        
+        /// <summary>
+        /// Luo uuden ruutukartan.
+        /// </summary>
+        /// <param name="tiles">Kaksiulotteinen taulukko merkeistä.</param>
+        public TileMap( char[,] tiles )
+            : base(tiles)
+        {
+        }
+
+        /// <summary>
+        /// Lukee ruutukentän tiedostosta.
+        /// </summary>
+        /// <param name="path">Tiedoston polku.</param>
+        public static TileMap FromFile( string path )
+        {
+            char[,] tiles = ReadFromFile( path );
+            return new TileMap( tiles );
+        }
+
+        /// <summary>
+        /// Lukee ruutukentän merkkijonotaulukosta.
+        /// </summary>
+        /// <param name="lines">Merkkijonotaulukko</param>        
+        public static TileMap FromStringArray(string[] lines)
+        {
+            char[,] tiles = ReadFromStringArray(lines);
+            return new TileMap(tiles);
+        }
+
+        /// <summary>
+        /// Lukee ruutukentän Content-projektin tekstitiedostosta.
+        /// </summary>
+        /// <param name="assetName">Tiedoston nimi</param>        
+        public static TileMap FromLevelAsset(string assetName)
+        {
+            string[] lines = Game.Instance.Content.Load<string[]>(assetName);
+            char[,] tiles = ReadFromStringArray(lines);
+            return new TileMap(tiles);
+        }
+        
+        /// <summary>
+        /// Asettaa oliot kenttään aiemmin annettujen merkkien perusteella.
+        /// </summary>
+        public void Insert()
+        {
+            double h = Game.Instance.Level.Height / tiles.GetLength(0);
+            double w = Game.Instance.Level.Width / tiles.GetLength(1);
+            Insert(w, h);
+        }
+
+        /// <summary>
+        /// Asettaa oliot kenttään aiemmin annettujen merkkien perusteella.
+        /// </summary>
+        /// <remarks>
+        /// Huom! Tiilien asettaminen muuttaa kentän kokoa. Jos lisäät kenttään reunat
+        /// tai zoomaat kameraa, tee se vasta tämän aliohjelman kutsun jälkeen.
+        /// </remarks>
+        /// <param name="tileWidth">Ruudun leveys.</param>
+        /// <param name="tileHeight">Ruudun korkeus.</param>
+        public void Insert( double tileWidth, double tileHeight )
+        {
+            Game game = Game.Instance;
+            int width = tiles.GetLength( 1 );
+            int height = tiles.GetLength( 0 );
+
+            game.Level.Width = width * tileWidth;
+            game.Level.Height = height * tileHeight;
+
+            for ( int y = height - 1; y >= 0; y-- )
+            {
+                for ( int x = 0; x < width; x++ )
+                {
+                    char symbol = tiles[y, x];
+                    if ( oldLegend.ContainsKey( symbol ) )
+                    {
+                        Func<GameObject> create = oldLegend[symbol];
+                        GameObject o = create();
+                        o.X = game.Level.Left + ( x * tileWidth ) + ( tileWidth / 2 );
+                        o.Y = game.Level.Top - ( y * tileHeight ) - ( tileHeight / 2 );
+                        game.Add( o );
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Lukee kentän ruudut tiedostosta.
+        /// </summary>
+        /// <param name="path">Tiedoston polku</param>
+        /// <returns>Kentän ruudut kaksiulotteisessa taulukossa</returns>
+        internal static char[,] ReadFromFile( string path )
+        {
+            var tileBuffer = new List<char[]>();            
+
+            using (StreamReader input = File.OpenText(path))
+            {
+                string line;
+                while ( ( line = input.ReadLine() ) != null )
+                {
+                    tileBuffer.Add( line.ToCharArray() );
+                }
+            }
+
+            return ListToArray(tileBuffer);
+        }
+
+        internal static char[,] ReadFromStringArray(string[] lines)
+        {
+            var tileBuffer = new List<char[]>();
+
+            for (int i = 0; i < lines.Length; i++ )
+            {
+
+                tileBuffer.Add(lines[i].ToCharArray());
+
+            }
+
+            return ListToArray(tileBuffer);
+        }   
+
+        private static char[,] ListToArray(List<char[]> list)
+        {
+            int finalWidth = list.Max(cs => cs.Length);
+
+            char[,] tiles = new char[list.Count, finalWidth];
+
+            for (int y = 0; y < list.Count; y++)
+            {
+                char[] row = list.ElementAt(y);
+
+                for (int x = 0; x < row.Length; x++)
+                {
+                    tiles[y, x] = row[x];
+                }
+            }
+
+            return tiles;
+        }
+    }
+}

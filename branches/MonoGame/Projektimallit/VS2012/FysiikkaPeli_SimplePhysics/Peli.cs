b85aad80c97ead6578ab0233017417ee2d997590
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-09-21 16:56:57 +0000

    Linux "project templates", changed MonoGame.Framework to Jypeli.MonoGame.Framework on Linux not to mix up with the original

--- branches/MonoGame/Projektimallit/VS2012/FysiikkaPeli_SimplePhysics/Peli.cs
+++ branches/MonoGame/Projektimallit/VS2012/FysiikkaPeli_SimplePhysics/Peli.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Jypeli;
-
-public class $safeprojectname$ : PhysicsGame
+﻿public class $safeprojectname$ : PhysicsGame
 {
     public override void Begin()
     {
+        // Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen( ConfirmExit, "Lopeta peli" );
+        Keyboard.Listen( Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli" );
     }
 }

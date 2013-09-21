f08a7ce8d3ed89706312f2c47bf2c552b1ef4fb7
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-09-21 16:56:57 +0000

    Linux "project templates", changed MonoGame.Framework to Jypeli.MonoGame.Framework on Linux not to mix up with the original

--- Projektimallit/Linux-MonoDevelop/PerusPeli/Peli.cs
+++ Projektimallit/Linux-MonoDevelop/PerusPeli/Peli.cs
@@ -1,4 +1,11 @@
-﻿public class $safeprojectname$ : PhysicsGame
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+public class Peli : Game
 {
     public override void Begin()
     {

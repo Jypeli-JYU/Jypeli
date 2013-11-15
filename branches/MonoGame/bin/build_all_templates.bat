c945b4009e9fe8262c94bd0bae7b23b517b2c4bf
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-11-15 18:36:58 +0000

    Build script builds both physics libraries for all platforms

--- branches/MonoGame/bin/build_all_templates.bat
+++ branches/MonoGame/bin/build_all_templates.bat
@@ -2,4 +2,5 @@
 setlocal
 call build_project_templates ..\Projektimallit\VS2012
 call build_project_templates ..\Projektimallit\WP8
+call build_project_templates ..\Projektimallit\Win8
 endlocal
--- branches/MonoGame/copy_dlls.bat
+++ branches/MonoGame/copy_dlls.bat
@@ -10,15 +10,8 @@ set copy_if_newer=xcopy /D /S /Q /I /Y
 if not exist %2 mkdir %2
 if not exist %2\Jypeli mkdir %2\Jypeli
 
-%copy_if_newer% %1\Jypeli.dll %2\Jypeli
-%copy_if_newer% %1\Jypeli.xml %2\Jypeli
-%copy_if_newer% %1\Jypeli.SimplePhysics.dll %2\Jypeli
-%copy_if_newer% %1\Jypeli.SimplePhysics.xml %2\Jypeli
-%copy_if_newer% %1\Jypeli.Physics2d.dll %2\Jypeli
-%copy_if_newer% %1\Jypeli.Physics2d.xml %2\Jypeli
-%copy_if_newer% %1\Jypeli.MonoGame.Framework.dll %2\Jypeli
-%copy_if_newer% %1\Lidgren.Network.dll %2\Jypeli
-%copy_if_newer% %1\Tao.Sdl.dll %2\Jypeli
+%copy_if_newer% %1\*.dll %2\Jypeli
+%copy_if_newer% %1\*.xml %2\Jypeli
 
 goto end
 

79fac28e9597fdc2c391a84abca0bed006b6af1f
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2013-11-21 00:50:48 +0000

    Build authoring, better build script

--- branches/MonoGame/build.bat
+++ branches/MonoGame/build.bat
@@ -2,59 +2,29 @@
 setlocal
 rem | Compiles the library
 
-set outputDir=Compiled
-set DefaultFrameworkDir=%WINDIR%\Microsoft.NET\Framework
-
-if not defined FrameworkDir (
-   set FrameworkDir=%DefaultFrameworkDir%
-)
-
-echo %FrameworkDir%
-if not exist %FrameworkDir%\v4.0.30319 (
-  ECHO.
-  ECHO.
-  echo .NET Framework 4.0 required.
-  ECHO.
-  ECHO.
-  goto error
-)
-
-
-set msbuild=%FrameworkDir%\v4.0.30319\MSBuild.exe
-
-%msbuild% Jypeli.proj
+call build_platform WindowsGL x86
 if errorlevel 1 goto error
 
-if not exist %outputDir% mkdir %outputDir%
+call build_platform Linux x86
+if errorlevel 1 goto error
 
-call copy_dlls Jypeli\bin\WindowsGL\Release %outputDir%\WindowsGL
-call copy_dlls Jypeli\bin\Linux\Release %outputDir%\Linux
-call copy_dlls Jypeli\bin\WindowsPhone\x86\Release %outputDir%\WP8-x86
-call copy_dlls Jypeli\bin\WindowsPhone\ARM\Release %outputDir%\WP8-ARM
-call copy_dlls Jypeli\bin\Windows8\x86\Release %outputDir%\Win8-x86
-call copy_dlls Jypeli\bin\Windows8\ARM\Release %outputDir%\Win8-ARM
+call build_platform WP8 x86
+if errorlevel 1 goto error
 
-call copy_dlls SimplePhysics\bin\WindowsGL\Release %outputDir%\WindowsGL
-call copy_dlls SimplePhysics\bin\Linux\Release %outputDir%\Linux
-call copy_dlls SimplePhysics\bin\WindowsPhone\x86\Release %outputDir%\WP8-x86
-call copy_dlls SimplePhysics\bin\WindowsPhone\ARM\Release %outputDir%\WP8-ARM
-call copy_dlls SimplePhysics\bin\Windows8\x86\Release %outputDir%\Win8-x86
-call copy_dlls SimplePhysics\bin\Windows8\ARM\Release %outputDir%\Win8-ARM
+call build_platform WP8 ARM
+if errorlevel 1 goto error
 
-call copy_dlls Physics2d\bin\WindowsGL\Release %outputDir%\WindowsGL
-call copy_dlls Physics2d\bin\Linux\Release %outputDir%\Linux
-call copy_dlls Physics2d\bin\WindowsPhone\x86\Release %outputDir%\WP8-x86
-call copy_dlls Physics2d\bin\WindowsPhone\ARM\Release %outputDir%\WP8-ARM
-call copy_dlls Physics2d\bin\Windows8\x86\Release %outputDir%\Win8-x86
-call copy_dlls Physics2d\bin\Windows8\ARM\Release %outputDir%\Win8-ARM
+call build_platform Win8 x86
+if errorlevel 1 goto error
+
+call build_platform Win8 ARM
+if errorlevel 1 goto error
 
 goto end
-
 
 :error
 endlocal
 exit /B 1
-
 
 :end
 endlocal

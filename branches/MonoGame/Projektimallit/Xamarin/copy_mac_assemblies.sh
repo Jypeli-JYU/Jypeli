f45120e3018711d58bec8fc2528bf9836f5fef4e
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2014-04-02 14:53:23 +0000

    Xamarin.Mac template

---
+++ branches/MonoGame/Projektimallit/Xamarin/copy_mac_assemblies.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp ../../Jypeli/bin/MacOS/Debug/*.dll assemblies/Mac/
+cp ../../Jypeli/bin/MacOS/Debug/*.xml assemblies/Mac/
+cp ../../SimplePhysics/bin/MacOS/Debug/Jypeli.SimplePhysics.dll assemblies/Mac/
+cp ../../SimplePhysics/bin/MacOS/Debug/Jypeli.SimplePhysics.xml assemblies/Mac/
+cp ../../Physics2d/bin/MacOS/Debug/Jypeli.Physics2d.dll assemblies/Mac/
+cp ../../Physics2d/bin/MacOS/Debug/Jypeli.Physics2d.xml assemblies/Mac/
+

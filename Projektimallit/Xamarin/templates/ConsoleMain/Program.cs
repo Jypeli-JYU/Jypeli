59385eb9eecb829c01d6681e602a719c5b60abea
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2014-09-18 21:50:16 +0000

    ConsoleMain project template for Mac

---
+++ Projektimallit/Xamarin/templates/ConsoleMain/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author ${User}
+/// @version ${Day}.${Month}.${Year}
+/// <summary>
+/// 
+/// </summary>
+public class ${ProjectName}
+{
+	public static void Main()
+	{
+	}
+}

3d9eec05940cffbafb1ab3ffa80141f11c3669e1
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2014-09-27 02:01:40 +0000

    Release 6.0.6 for MacOS

--- branches/MonoGame/Projektimallit/Xamarin/copy_mac_assemblies.sh
+++ branches/MonoGame/Projektimallit/Xamarin/copy_mac_assemblies.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-cp ../../Jypeli/bin/MacOS/Debug/*.dll assemblies/Mac/
-cp ../../Jypeli/bin/MacOS/Debug/*.xml assemblies/Mac/
-cp ../../SimplePhysics/bin/MacOS/Debug/Jypeli.SimplePhysics.dll assemblies/Mac/
-cp ../../SimplePhysics/bin/MacOS/Debug/Jypeli.SimplePhysics.xml assemblies/Mac/
-cp ../../Physics2d/bin/MacOS/Debug/Jypeli.Physics2d.dll assemblies/Mac/
-cp ../../Physics2d/bin/MacOS/Debug/Jypeli.Physics2d.xml assemblies/Mac/
+cp ../../Jypeli/bin/MacOS/Release/*.dll assemblies/Mac/
+cp ../../Jypeli/bin/MacOS/Release/*.xml assemblies/Mac/
+cp ../../SimplePhysics/bin/MacOS/Release/Jypeli.SimplePhysics.dll assemblies/Mac/
+cp ../../SimplePhysics/bin/MacOS/Release/Jypeli.SimplePhysics.xml assemblies/Mac/
+cp ../../Physics2d/bin/MacOS/Release/Jypeli.Physics2d.dll assemblies/Mac/
+cp ../../Physics2d/bin/MacOS/Release/Jypeli.Physics2d.xml assemblies/Mac/
 

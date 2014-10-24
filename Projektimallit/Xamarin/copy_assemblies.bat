1235898957da85d75880dc8774d9e35b643f5d93
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2014-10-24 00:23:25 +0000

    Script updates

--- Projektimallit/Xamarin/copy_assemblies.bat
+++ Projektimallit/Xamarin/copy_assemblies.bat
@@ -1,3 +1,6 @@
 @echo off
-mkdir MonoDevelop.Jypeli.Windows\assemblies\WindowsGL
-copy ..\..\Compiled\WindowsGL-x86\*.* MonoDevelop.Jypeli.Windows\assemblies\WindowsGL
+mkdir assemblies\WindowsGL
+copy ..\..\Compiled\WindowsGL-x86\*.* assemblies\WindowsGL
+
+mkdir assemblies\Linux
+copy ..\..\Compiled\Linux-x86\*.* assemblies\Linux

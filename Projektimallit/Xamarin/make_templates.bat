72508b70f48604e4b1823c25230102ea42bbd5bb
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2014-11-06 18:43:03 +0000

    Updated template build script

--- Projektimallit/Xamarin/make_templates.bat
+++ Projektimallit/Xamarin/make_templates.bat
@@ -22,23 +22,37 @@ set msbuild=%FrameworkDir%\v4.0.30319\MSBuild.exe
 
 call copy_assemblies.bat
 mkdir bin\mpack
+mkdir bin\mpack\MonoDevelop-4.0
+mkdir bin\mpack\MonoDevelop-4.0\Linux
+mkdir bin\mpack\MonoDevelop-5.0\Linux
+mkdir bin\mpack\MonoDevelop-5.0\Windows
 
 %msbuild% /p:Configuration=Release MonoDevelop.Jypeli.Windows.sln
 pushd bin\Windows\Release
 %mdtool% setup pack MonoDevelop.Jypeli.Windows.dll
 popd
-for %%f in (bin\Windows\Release\*.mpack) do move %%f bin\mpack\%%~Nf_Windows.mpack
+move bin\Windows\Release\*.mpack bin\mpack\MonoDevelop-5.0\Windows\
 
 %msbuild% /p:Configuration=Release MonoDevelop.Jypeli.Linux.v4.sln
 pushd bin\Linux\Release-v4
 %mdtool% setup pack MonoDevelop.Jypeli.Linux.dll
 popd
-for %%f in (bin\Linux\Release-v4\*.mpack) do move %%f bin\mpack\%%~Nf_Linux.v4.mpack
+move bin\Linux\Release-v4\*.mpack bin\mpack\MonoDevelop-4.0\Linux\
 
 %msbuild% /p:Configuration=Release MonoDevelop.Jypeli.Linux.sln
 pushd bin\Linux\Release
 %mdtool% setup pack MonoDevelop.Jypeli.Linux.dll
 popd
-for %%f in (bin\Linux\Release\*.mpack) do move %%f bin\mpack\%%~Nf_Linux.mpack
+move bin\Linux\Release\*.mpack bin\mpack\MonoDevelop-5.0\Linux\
+
+%mdtool% setup rep-build bin\mpack\MonoDevelop-4.0\Linux
+%mdtool% setup rep-build bin\mpack\MonoDevelop-5.0\Linux
+%mdtool% setup rep-build bin\mpack\MonoDevelop-5.0\Windows
+%mdtool% setup rep-build bin\mpack\MonoDevelop-5.0\Mac
+
+del bin\mpack\MonoDevelop-4.0\Linux\index.html
+del bin\mpack\MonoDevelop-5.0\Linux\index.html
+del bin\mpack\MonoDevelop-5.0\Windows\index.html
+del bin\mpack\MonoDevelop-5.0\Mac\index.html
 
 endlocal

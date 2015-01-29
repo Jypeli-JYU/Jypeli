f796c27848d44c71486e1e3d6a483a54b7c8083d
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-01-29 18:21:19 +0000

    Added a non-versioned file

---
+++ branches/MonoGame/Physics2d/PhysicsGame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jypeli.Physics;
+using Jypeli.Physics2d;
+
+namespace Jypeli
+{
+    /// <summary>
+    /// Peli, jossa on fysiikan laskenta mukana. Peliin lisätyt <code>PhysicsObject</code>-oliot
+    /// käyttäytyvät fysiikan lakien mukaan.
+    /// </summary>
+    public class PhysicsGame : PhysicsGameBase
+    {
+        /// <summary>
+        /// Käynnissä olevan fysiikkapelin pääolio.
+        /// </summary>
+        public static new PhysicsGame Instance
+        {
+            get
+            {
+                if ( Game.Instance == null ) throw new InvalidOperationException( "Game class is not initialized" );
+                if ( !( Game.Instance is PhysicsGameBase ) ) throw new InvalidOperationException( "Game class is not PhysicsGame" );
+
+                return (PhysicsGame)( Game.Instance );
+            }
+        }
+
+        /// <summary>
+        /// Alustaa uuden fysiikkapelin.
+        /// </summary>
+        public PhysicsGame()
+            : base( new Physics2dEngine() )
+        {
+        }
+    }
+}

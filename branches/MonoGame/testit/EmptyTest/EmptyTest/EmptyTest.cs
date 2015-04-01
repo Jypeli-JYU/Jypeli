cea35cc99c3e4dc929f04599a4252a612d917b63
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-04-01 19:41:49 +0000

    Empty MonoJypeli (VS2013) project with references for quick testing

---
+++ branches/MonoGame/testit/EmptyTest/EmptyTest/EmptyTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+//using Jypeli.Effects;
+using Jypeli.Widgets;
+
+public class EmptyTest : PhysicsGame
+{
+    public override void Begin()
+    {
+        // TODO: Kirjoita ohjelmakoodisi tähän
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+}

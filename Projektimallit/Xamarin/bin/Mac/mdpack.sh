cc96bf76c2cf30ac43d8b60c0dc141ebaef5b309
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-06-25 16:35:50 +0000

    Automated build for MacOS

--- Projektimallit/Xamarin/bin/Mac/mdpack.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-MDTOOL="/Applications/Xamarin Studio.app/Contents/MacOS/mdtool"
-mkdir mpack
-rm mpack/*
-"${MDTOOL}" setup pack Release/MonoDevelop.Jypeli.Mac.dll && mv *.mpack mpack && "${MDTOOL}" setup rep-build mpack
-rm mpack/index.html

12b17f1d3a40eb61ce054f3072db8b5c44a39806
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-06 19:06:27 +0000

    Automatically apply patches to MonoGame when running module_init.sh

--- branches/MonoGame/MonoGame/module_init.sh
+++ branches/MonoGame/MonoGame/module_init.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 git init && git remote add -t \* -f origin https://github.com/mono/MonoGame.git && git pull origin master && git submodule update --init --recursive ThirdParty
-
+for file in `find . -name '*.cs.diff'`
+do
+   patch `echo $file | sed -e 's/\.diff//g'` $file
+done

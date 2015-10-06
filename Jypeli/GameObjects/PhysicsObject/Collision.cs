929ef38402ee875c1fc552c8c0a4e8f94b21054f
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-06 16:37:39 +0000

    Added a missing file

---
+++ Jypeli/GameObjects/PhysicsObject/Collision.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Jypeli.Physics
+{
+    public class Collision
+    {        
+        public class ContactPoint
+        {
+            public Vector Position { get; set; }
+            public Vector Normal { get; set; }
+
+            public ContactPoint(Vector pos, Vector n)
+            {
+                Position = pos;
+                Normal = n;
+            }
+        }
+
+        public IPhysicsBody Object1 { get; private set; }
+        public IPhysicsBody Object2 { get; private set; }
+        public IEnumerable<ContactPoint> Contacts { get; private set; }
+
+        public Collision(IPhysicsBody o1, IPhysicsBody o2, IEnumerable<ContactPoint> contacts)
+        {
+            Object1 = o1;
+            Object2 = o2;
+            Contacts = contacts;
+        }
+    }
+}

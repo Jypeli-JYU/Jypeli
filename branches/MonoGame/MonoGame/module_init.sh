72f6a12065e278857540783687d039204e25bc20
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-07 22:31:55 +0000

    Updated module_init.sh so it can be called again to update monogame

--- branches/MonoGame/MonoGame/module_init.sh
+++ branches/MonoGame/MonoGame/module_init.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-git init && git remote add -t \* -f origin https://github.com/mono/MonoGame.git && git pull origin master && git submodule update --init --recursive ThirdParty
+
+# First run: clone the repository
+git init && git remote add -t \* -f origin https://github.com/mono/MonoGame.git && git branch --set-upstream-to=origin/master master
+
+# Subsequent runs: stash changes, update everything, pop stash, apply new patches
+git stash && git pull && git submodule update --init --recursive . && git stash pop
 for file in `find . -name '*.cs.diff'`
 do
-   patch `echo $file | sed -e 's/\.diff//g'` $file
+   patch -N -r - `echo $file | sed -e 's/\.diff//g'` $file
 done

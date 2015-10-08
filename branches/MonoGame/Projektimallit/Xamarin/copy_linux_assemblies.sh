3f0f5285a140d744de38e0088ef80d648a74d1b1
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-08 00:09:04 +0000

    Upgraded Linux addin project to the new 1.2 addin format

---
+++ branches/MonoGame/Projektimallit/Xamarin/copy_linux_assemblies.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+mkdir -p assemblies/Linux
+cp ../../Jypeli/bin/Linux/AnyCPU/Release/* assemblies/Linux/
+cp ../../SimplePhysics/bin/Linux/AnyCPU/Release/* assemblies/Linux/
+cp ../../Physics2d/bin/Linux/AnyCPU/Release/* assemblies/Linux/
+

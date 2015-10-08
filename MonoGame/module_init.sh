e6671e7e609d8e938f90bce2dea208c99a04e38f
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-08 01:14:00 +0000

    Windows git compatibility

--- MonoGame/module_init.sh
+++ MonoGame/module_init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # First run: clone the repository
-git init && git remote add -t \* -f origin https://github.com/mono/MonoGame.git && git branch --set-upstream-to=origin/master master
+git init && git remote add -t \* -f origin https://github.com/mono/MonoGame.git
 
 # Subsequent runs: stash changes, update everything, pop stash, apply new patches
-git stash && git pull && git submodule update --init --recursive . && git stash pop
+git stash && git pull origin master && git submodule update --init --recursive . && git stash pop
 for file in `find . -name '*.cs.diff'`
 do
    patch -N -r - `echo $file | sed -e 's/\.diff//g'` $file

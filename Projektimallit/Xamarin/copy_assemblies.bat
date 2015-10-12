f71d6119d7aefd91e734ebbddd1376ea2e1fbfab
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2015-10-12 21:52:47 +0000

    Working Windows/OpenGL Xamarin template

--- Projektimallit/Xamarin/copy_assemblies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-mkdir assemblies\Windows
-copy ..\..\Compiled\Windows-AnyCPU\*.* assemblies\Windows
-
-mkdir assemblies\WindowsGL
-copy ..\..\Compiled\WindowsGL-AnyCPU\*.* assemblies\WindowsGL
-
-mkdir assemblies\Linux
-copy ..\..\Compiled\Linux-AnyCPU\*.* assemblies\Linux

05c91e412bd74fce64b5a31c5add177e3fd2f691
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-09-28 20:48:46 +0000

    Added physics templates to Xamarin 6.1.0 templates

--- branches/MonoGame/Projektimallit/Xamarin/6.1.0/Windows/Properties/AddinInfo.cs
+++ branches/MonoGame/Projektimallit/Xamarin/6.1.0/Windows/Properties/AddinInfo.cs
@@ -3,7 +3,7 @@ using Mono.Addins;
 using Mono.Addins.Description;
 
 [assembly: Addin(
-	"MonoDevelop.Jypeli.Windows",
+	"Jypeli",
 	Namespace = "Jypeli.Windows",
 	Version = "6.8.0"
 )]
@@ -11,7 +11,7 @@ using Mono.Addins.Description;
 [assembly:AddinDependency ("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
 [assembly:AddinDependency ("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
 
-[assembly: AddinName("MonoDevelop.Jypeli.Windows")]
+[assembly: AddinName("Jypeli (DirectX, experimental)")]
 [assembly: AddinCategory("Jypeli")]
-[assembly: AddinDescription("MonoDevelop.Jypeli.Windows")]
+[assembly: AddinDescription("Jypeli game programming library, experimental DirectX port")]
 [assembly: AddinAuthor("Jyväskylän yliopisto")]

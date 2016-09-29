302bc402baa6444611844d9faece4ad5ea52869d
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-09-29 23:30:24 +0000

    NuGet packages for Windows and Linux, makefile
    NuGet packages for Windows and Linux and a makefile for them.
    Updated Physics2d and SimplePhysics packages to work with
    both of them.

--- branches/MonoGame/NuGet/build.bat
+++ branches/MonoGame/NuGet/build.bat
@@ -1,21 +1,30 @@
 @echo off
 setlocal
 
-mkdir Jypeli\lib
-mkdir Jypeli\content
+mkdir Jypeli.WindowsGL\lib
+mkdir Jypeli.Linux\lib
 mkdir Jypeli.Physics2d\lib
-mkdir Jypeli.Physics2d\content
 mkdir Jypeli.SimplePhysics\lib
-mkdir Jypeli.SimplePhysics\content
 
-copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.dll Jypeli\lib
-copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.xml Jypeli\content
+copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.dll Jypeli.WindowsGL\lib
+copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.xml Jypeli.WindowsGL\content
 copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.Physics2d.dll Jypeli.Physics2d\lib
 copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.Physics2d.xml Jypeli.Physics2d\content
 copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.SimplePhysics.dll Jypeli.SimplePhysics\lib
 copy ..\Compiled\WindowsGL-AnyCPU\Jypeli.SimplePhysics.xml Jypeli.SimplePhysics\content
 
-pushd Jypeli
+copy ..\Compiled\Linux-AnyCPU\Jypeli.dll Jypeli.Linux\lib
+copy ..\Compiled\Linux-AnyCPU\Jypeli.xml Jypeli.Linux\content
+copy ..\Compiled\Linux-AnyCPU\Jypeli.Physics2d.dll Jypeli.Physics2d\lib
+copy ..\Compiled\Linux-AnyCPU\Jypeli.Physics2d.xml Jypeli.Physics2d\content
+copy ..\Compiled\Linux-AnyCPU\Jypeli.SimplePhysics.dll Jypeli.SimplePhysics\lib
+copy ..\Compiled\Linux-AnyCPU\Jypeli.SimplePhysics.xml Jypeli.SimplePhysics\content
+
+pushd Jypeli.WindowsGL
+..\nuget pack
+popd
+
+pushd Jypeli.Linux
 ..\nuget pack
 popd
 

96f96b077cf2f215c682e3d1ee72c40a7aafcd78
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-10-04 21:31:09 +0000

    NuGet project templates for Linux (WIP)

---
+++ branches/MonoGame/Projektimallit/Xamarin-NuGet/Common/templates/ConsoleMain/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author ${User}
+/// @version ${Day}.${Month}.${Year}
+/// <summary>
+/// 
+/// </summary>
+public class ${ProjectName}
+{
+	public static void Main()
+	{
+		// Kirjoita ohjelmakoodisi tähän
+	}
+}
---
+++ branches/MonoGame/Projektimallit/Xamarin-NuGet/Jypeli.Linux/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"Jypeli", 
+	Namespace = "Jypeli.Linux",
+	Version = "6.8.0"
+)]
+
+[assembly:AddinName ("Jypeli")]
+[assembly:AddinCategory ("Jypeli")]
+[assembly:AddinDescription ("Jypeli game programming library for Linux")]
+[assembly:AddinAuthor ("Jyväskylän yliopisto")]
+

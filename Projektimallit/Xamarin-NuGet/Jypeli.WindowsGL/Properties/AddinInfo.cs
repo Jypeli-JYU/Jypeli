77ca6dfb62935b3deaca3f4c9e0e2fe895f05233
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-10-12 16:00:07 +0000

    Windows NuGet project templates

---
+++ Projektimallit/Xamarin-NuGet/Jypeli.WindowsGL/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+	"Jypeli",
+	Namespace = "Jypeli.WindowsGL",
+	Version = "6.8.0"
+)]
+
+[assembly: AddinName("Jypeli")]
+[assembly: AddinCategory("Jypeli")]
+[assembly: AddinDescription("Jypeli game programming library for Windows")]
+[assembly: AddinAuthor("Jyväskylän yliopisto")]
+

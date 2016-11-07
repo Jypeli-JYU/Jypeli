125f16111332b0f2b408713854e3afb529762e05
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-11-07 21:00:12 +0000

    First MonoMac template that runs on XS6
    Templates now produce a running executable on Xamarin Studio 6.
    For now, the game crashes when trying to add objects or load content.

---
+++ branches/MonoGame/Projektimallit/Xamarin-NuGet/Jypeli.MacOS/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin (
+    "Jypeli",
+    Namespace = "Jypeli.MacOS",
+    Version = "6.8.0"
+)]
+
+[assembly: AddinName ("Jypeli")]
+[assembly: AddinCategory ("Jypeli")]
+[assembly: AddinDescription ("Jypeli game programming library for Mac OS X")]
+[assembly: AddinAuthor ("Jyväskylän yliopisto")]
+

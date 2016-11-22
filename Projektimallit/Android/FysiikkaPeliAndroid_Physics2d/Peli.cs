f58c28425d681b4fe4925f80b078b4b2805efe6d
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-11-22 13:32:36 +0000

    Android physics game templates for Visual Studio

---
+++ Projektimallit/Android/FysiikkaPeliAndroid_Physics2d/Peli.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+namespace $safeprojectname$
+{
+    public class $safeprojectname$ : PhysicsGame
+    {
+        public override void Begin()
+        {
+            // Kirjoita ohjelmakoodisi tähän
+
+            PhoneBackButton.Listen( ConfirmExit, "Lopeta peli" );
+            Keyboard.Listen( Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli" );
+        }
+    }
+}

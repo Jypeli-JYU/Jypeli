b3e6564a59daccd701509a586a6daed835e854af
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2016-11-23 16:46:20 +0000

    Content working in Xamarin Mac template
    Graphics and text files work now. Audio doesn't because of a bug
    in MonoGame.

--- branches/MonoGame/Projektimallit/Xamarin-NuGet/Jypeli.MacOS/JypeliProjectExtension.cs
+++ branches/MonoGame/Projektimallit/Xamarin-NuGet/Jypeli.MacOS/JypeliProjectExtension.cs
@@ -2,128 +2,157 @@
 using MonoDevelop.Ide;
 using MonoDevelop.Projects;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 
 namespace Jypeli.Projects
 {
-	public class JypeliProjectExtension : DotNetProjectExtension
-	{
-		private FilePath openFile = null;
+    public class JypeliProjectExtension : DotNetProjectExtension
+    {
+        private FilePath openFile = null;
 
-		public JypeliProjectExtension()
-			: base()
-		{
-		}
+        public JypeliProjectExtension()
+            : base()
+        {
+        }
 
-		private bool IsJypeliProject(Project project)
-		{
-			foreach (var item in project.Items)
-			{
-				if (item.ItemName == "Reference" && item.Include == "Jypeli")
-					return true;
-			}
+        private bool IsJypeliProject(Project project)
+        {
+            foreach (var item in project.Items)
+            {
+                if (item.ItemName == "Reference" && item.Include == "Jypeli")
+                    return true;
+            }
 
-			return false;
-		}
+            return false;
+        }
 
-		private bool IsLibraryFile(FilePath file)
-		{
-			return file.Extension == ".dll" ||
-		           file.FileName.EndsWith(".dll.config", StringComparison.InvariantCulture);
-		}
+        private bool IsLibraryFile(FilePath file)
+        {
+            return file.Extension == ".dll" ||
+                   file.FileName.EndsWith(".dll.config", StringComparison.InvariantCulture);
+        }
 
-		private bool IsContentFile(FilePath file)
-		{
-			return !file.IsDirectory && file.IsChildPathOf(Project.ItemDirectory.Combine("Content"));
-		}
+        private bool IsContentFile(FilePath file)
+        {
+            return !file.IsDirectory && file.IsChildPathOf(Project.ItemDirectory.Combine("Content"));
+        }
 
-		protected override void OnItemsAdded(System.Collections.Generic.IEnumerable<ProjectItem> objs)
-		{
-			if (IsJypeliProject(Project))
-			{
-				if (this.openFile != null)
-				{
-					var file = this.openFile;
-					this.openFile = null;
+        protected override void OnItemsAdded(System.Collections.Generic.IEnumerable<ProjectItem> objs)
+        {
+            if (IsJypeliProject(Project))
+            {
+                if (this.openFile != null)
+                {
+                    var file = this.openFile;
+                    this.openFile = null;
                     Runtime.RunInMainThread(() => IdeApp.Workbench.OpenDocument(file, Project, true));
-				}
-			}
+                }
+            }
 
-			base.OnItemsAdded(objs);
-		}
+            base.OnItemsAdded(objs);
+        }
 
-		protected override Task<BuildResult> OnBuild(ProgressMonitor monitor, ConfigurationSelector configuration, OperationContext operationContext)
-		{
-			if (IsJypeliProject(Project))
-			{
+        protected override Task<BuildResult> OnBuild(ProgressMonitor monitor, ConfigurationSelector configuration, OperationContext operationContext)
+        {
+            if (IsJypeliProject(Project))
+            {
 #if !NOMGCB
-				var contentDir = Project.ItemDirectory.Combine("Content");
-				var outputDir = Project.GetOutputFileName(configuration).ParentDirectory.CanonicalPath.Combine("Content").ToString();
+                var oldXnbFiles = new List<String>();
+                var xnbFiles = new List<FilePath>();
+                var contentDir = Project.ItemDirectory.Combine("Content");
+                //var outputDir = Project.GetOutputFileName(configuration).ParentDirectory.CanonicalPath.Combine("Content").ToString();
                 MGCB mgcb = null;
 
-				try
-				{
-					mgcb = new MGCB(contentDir, outputDir);
-				}
-				catch (NotSupportedException e)
-				{
-					monitor.Log.WriteLine("Could not initialize content builder - " + e.Message);
-				}
+                try
+                {
+                    //mgcb = new MGCB(contentDir, outputDir);
+                    mgcb = new MGCB(contentDir, contentDir);
+                }
+                catch (NotSupportedException e)
+                {
+                    monitor.Log.WriteLine("Could not initialize content builder - " + e.Message);
+                }
 #endif
 
-				foreach (var item in Project.Items)
-				{
-					var fileItem = item as ProjectFile;
-					if (fileItem == null)
-						continue;
+                foreach (var item in Project.Items)
+                {
+                    var fileItem = item as ProjectFile;
+                    if (fileItem == null)
+                        continue;
 
-					// Copy DLL files to output directory
-					if (IsLibraryFile(fileItem.FilePath))
-						fileItem.CopyToOutputDirectory = FileCopyMode.PreserveNewest;
+                    // Copy DLL files to output directory
+                    if (IsLibraryFile(fileItem.FilePath))
+                        fileItem.CopyToOutputDirectory = FileCopyMode.PreserveNewest;
 
                     // Mark dylib files as native references
                     if (fileItem.FilePath.Extension == ".dylib")
                         fileItem.BuildAction = "NativeReference";
 
-					// Mark content files as content and copy to output directory
-					if (IsContentFile(fileItem.FilePath))
-					{
+                    // Mark content files as content and copy to output directory
+                    if (IsContentFile(fileItem.FilePath))
+                    {
                         fileItem.BuildAction = "Content";
                         fileItem.CopyToOutputDirectory = FileCopyMode.PreserveNewest;
 
 #if !NOMGCB
-                        if (mgcb != null && fileItem.Name.EndsWith (".wav", StringComparison.InvariantCulture)) {
-                            // Sound effect
-                            fileItem.CopyToOutputDirectory = FileCopyMode.None;
-                            mgcb.BuildContent (monitor, fileItem.FilePath, "WavImporter", "SoundEffectProcessor");
-                        } else if (mgcb != null && fileItem.Name.EndsWith (".mp3", StringComparison.InvariantCulture)) {
-                            // Music
-                            fileItem.CopyToOutputDirectory = FileCopyMode.None;
-                            mgcb.BuildContent (monitor, fileItem.FilePath, "Mp3Importer", "SongProcessor");
-						}
-                        else
+                        if (fileItem.FilePath.Extension == ".xnb")
                         {
+                            oldXnbFiles.Add(fileItem.FilePath.CanonicalPath.ToString());
+                        }
+                        else if (mgcb != null)
+                        {
+                            string importer = null;
+                            string processor = null;
+                            string assembly = null;
+
+                            fileItem.BuildAction = BuildAction.None;
                             fileItem.CopyToOutputDirectory = FileCopyMode.None;
-                            mgcb.BuildContent (monitor, fileItem.FilePath);
+
+                            switch (fileItem.FilePath.Extension)
+                            {
+                                case ".wav":
+                                    importer = "WavImporter";
+                                    processor = "SoundEffectProcessor";
+                                    break;
+                                case ".mp3":
+                                    importer = "Mp3Importer";
+                                    processor = "SongProcessor";
+                                    break;
+                                case ".txt":
+                                    importer = "TextFileImporter";
+                                    processor = "TextFileContentProcessor";
+                                    assembly = "TextFileContentExtension.dll";
+                                    break;
+                            }
+
+                            var xnbFile = mgcb.BuildContent(monitor, fileItem.FilePath, importer, processor, assembly);
+                            xnbFiles.Add(xnbFile);
                         }
 #endif
-					}
-				}
-			}
+                    }
+                }
 
-			return base.OnBuild(monitor, configuration, operationContext);
-		}
+#if !NOMGCB
+                xnbFiles.RemoveAll((f) => oldXnbFiles.Contains(f.CanonicalPath.ToString()));
+                foreach (var pf in Project.AddFiles(xnbFiles, "Content"))
+                    pf.CopyToOutputDirectory = FileCopyMode.PreserveNewest;
+#endif
+            }
 
-		protected override ProjectItem OnCreateProjectItem(MonoDevelop.Projects.MSBuild.IMSBuildItemEvaluated item)
-		{
-			if (item.Include == Project.Name + ".cs")
-			{
-				// Open the main .cs file in editor
-				this.openFile = Project.ItemDirectory.Combine(new FilePath(item.Include));
-			}
+            return base.OnBuild(monitor, configuration, operationContext);
+        }
 
-			return base.OnCreateProjectItem(item);
-		}
-	}
+        protected override ProjectItem OnCreateProjectItem(MonoDevelop.Projects.MSBuild.IMSBuildItemEvaluated item)
+        {
+            if (item.Include == Project.Name + ".cs")
+            {
+                // Open the main .cs file in editor
+                this.openFile = Project.ItemDirectory.Combine(new FilePath(item.Include));
+            }
+
+            return base.OnCreateProjectItem(item);
+        }
+    }
 }

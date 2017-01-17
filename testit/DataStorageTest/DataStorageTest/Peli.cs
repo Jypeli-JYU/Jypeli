826f3a2b4c630c71e9c8b4585416cab6192e9013
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2017-01-17 23:40:55 +0000

    (MonoGame) Added high score list testing to data storage tests. Tests now pass on Win8.1 and WP8.1 platforms, though saved fields and properties have to be declared public.

--- testit/DataStorageTest/DataStorageTest/Peli.cs
+++ testit/DataStorageTest/DataStorageTest/Peli.cs
@@ -12,9 +12,10 @@ public class Peli : Game
         Level.BackgroundColor = Color.Gray;
 
         //MessageDisplay.Position = new Vector(100, 100);
-        MessageDisplay.MaxMessageCount = 30;
+        MessageDisplay.MaxMessageCount = (int)( Screen.Height / Font.Default.CharacterHeight - 1 );
         MessageDisplay.BackgroundColor = Color.Transparent;
         MessageDisplay.MessageTime = TimeSpan.MaxValue;
+        MessageDisplay.RealTime = true;
         MessageDisplay.TextColor = Color.LightGray;
 
         Keyboard.Listen( Key.Escape, ButtonState.Pressed, Exit, null );
@@ -24,28 +25,30 @@ public class Peli : Game
 
         try
         {
-            tiedosto = DataStorage.Create("testi.txt");
-            Assert(tiedosto.Name.EndsWith("testi.txt"), "tiedosto.Name.EndsWith(\"testi.txt\")");
-            AssertNotNull(tiedosto.Stream, "tiedosto.Stream");
+            DataStorage.Delete( "testi.txt" );
+
+            tiedosto = DataStorage.Create( "testi.txt" );
+            Assert( tiedosto.Name.EndsWith( "testi.txt" ), "tiedosto.Name.EndsWith(\"testi.txt\")" );
+            AssertNotNull( tiedosto.Stream, "tiedosto.Stream" );
             tiedosto.Close();
 
-            Assert(DataStorage.Exists("testi.txt"), "DataStorage.Exists(\"testi.txt\")");
+            Assert( DataStorage.Exists( "testi.txt" ), "DataStorage.Exists(\"testi.txt\")" );
 
-            tiedosto = DataStorage.Open("testi.txt", false);
-            Assert(tiedosto.Name.EndsWith("testi.txt"), "tiedosto.Name.EndsWith(\"testi.txt\")");
-            AssertNotNull(tiedosto.Stream, "tiedosto.Stream");
-            Assert(!tiedosto.Stream.CanWrite, "!tiedosto.Stream.CanWrite");
+            tiedosto = DataStorage.Open( "testi.txt", false );
+            Assert( tiedosto.Name.EndsWith( "testi.txt" ), "tiedosto.Name.EndsWith(\"testi.txt\")" );
+            AssertNotNull( tiedosto.Stream, "tiedosto.Stream" );
+            Assert( !tiedosto.Stream.CanWrite, "!tiedosto.Stream.CanWrite" );
             tiedosto.Close();
 
-            DataStorage.Delete("testi.txt");
-            Assert(!DataStorage.Exists("testi.txt"), "!DataStorage.Exists(\"testi.txt\")");
+            DataStorage.Delete( "testi.txt" );
+            Assert( !DataStorage.Exists( "testi.txt" ), "!DataStorage.Exists(\"testi.txt\")" );
 
-            MessageDisplay.Add("---");
+            MessageDisplay.Add( "---" );
 
-            DataStorage.MkDir("kansio");
-            Assert(DataStorage.Exists("kansio"), "DataStorage.Exists(\"kansio\")");
+            DataStorage.MkDir( "kansio" );
+            Assert( DataStorage.Exists( "kansio" ), "DataStorage.Exists(\"kansio\")" );
             Assert( DataStorage.ChDir( "kansio" ) == true, "DataStorage.ChDir( \"kansio\" ) == true" );
-            AssertValue(DataStorage.GetFileList().Count, 0, "DataStorage.GetFileList().Count");
+            AssertValue( DataStorage.GetFileList().Count, 0, "DataStorage.GetFileList().Count" );
             Assert( DataStorage.Exists( "kansio" ) == false, "DataStorage.Exists(\"kansio\") == false" );
             Assert( DataStorage.ChDir( ".." ) == true, "DataStorage.ChDir( \"..\" ) == true" );
             Assert( DataStorage.Exists( "kansio" ) == true, "DataStorage.Exists(\"kansio\") == true" );
@@ -54,43 +57,62 @@ public class Peli : Game
             Assert( DataStorage.Exists( "kansio" ) == false, "DataStorage.Exists(\"kansio\") == false" );
             Assert( DataStorage.ChDir( "kansio" ) == false, "DataStorage.ChDir( \"kansio\" ) == false" );
 
-            MessageDisplay.Add("---");
-            MessageDisplay.Add("PASSED!", Color.LimeGreen);
+            DataStorage.Delete( "topten.xml" );
+            Assert( !DataStorage.Exists( "topten.xml" ), "!DataStorage.Exists(\"topten.xml\")" );
+            ScoreList score = new ScoreList();
+            score.Add( "sata", 100 );
+            score.Add( "yksi", 1 );
+            score.Add( "kaksi", 2 );
+            DataStorage.Save( score, "topten.xml" );
+            Assert( DataStorage.Exists( "topten.xml" ), "DataStorage.Exists(\"topten.xml\")" );
+
+            ScoreList score2 = new ScoreList();
+            score2 = DataStorage.Load( score2, "topten.xml" );
+            Assert( score2.Equals( score ), "score2.Equals( score )" );
+
+            MessageDisplay.Add( "---" );
+            MessageDisplay.Add( "PASSED!", Color.LimeGreen );
         }
-        catch (TestException)
+        catch ( TestException )
         {
-            if (tiedosto != null && tiedosto.Stream.CanRead)
+            MessageDisplay.Add( "---" );
+            MessageDisplay.Add( "FAILED!", Color.Red );
+        }
+        finally
+        {
+            if ( tiedosto != null && tiedosto.Stream.CanRead )
                 tiedosto.Close();
 
-            DataStorage.Delete("testi.txt");
+            DataStorage.Delete( "testi.txt" );
+            DataStorage.Delete( "topten.xml" );
         }
     }
 
-    void Assert(bool ehto, string message)
+    void Assert( bool ehto, string message )
     {
         Color color = ehto ? Color.LightGreen : Color.Red;
-        MessageDisplay.Add(message, color);
+        MessageDisplay.Add( message, color );
 
-        if (!ehto)
+        if ( !ehto )
             throw new TestException();
     }
 
-    void AssertNotNull(object obj, string objName)
+    void AssertNotNull( object obj, string objName )
     {
-        if (obj != null)
+        if ( obj != null )
         {
-            MessageDisplay.Add(objName + " == " + obj.ToString(), Color.LightGreen);
+            MessageDisplay.Add( objName + " == " + obj.ToString(), Color.LightGreen );
         }
         else
         {
-            MessageDisplay.Add(objName + " == NULL", Color.Red);
+            MessageDisplay.Add( objName + " == NULL", Color.Red );
             throw new TestException();
         }
     }
 
-    void AssertValue<T>(T varValue, T expected, string varName) where T : IEquatable<T>
+    void AssertValue<T>( T varValue, T expected, string varName ) where T : IEquatable<T>
     {
-        Assert(varValue.Equals(expected), varName + " == " + expected);
+        Assert( varValue.Equals( expected ), varName + " == " + expected );
     }
 
     private class TestException : Exception

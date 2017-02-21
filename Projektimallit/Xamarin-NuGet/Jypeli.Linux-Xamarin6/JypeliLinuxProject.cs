e5cae28214e2c1c97b2179ff3b5adc570a3f1d9a
tojukarp <tojukarp@d9b0b244-d62e-4533-a3d1-d6789e1a15d5>
2017-02-21 09:45:24 +0000

    Two Xamarin plugin for Linux
    One targeting the new version 6 and the other targeting the old version 5.
    Some may still prefer to use the old version, as the new one can only run
    in a sandboxed environment unless built from source.

---
+++ Projektimallit/Xamarin-NuGet/Jypeli.Linux-Xamarin6/JypeliLinuxProject.cs
@@ -0,0 +1,31 @@
+﻿using MonoDevelop.Core;
+using MonoDevelop.Projects;
+using System;
+using System.Xml;
+
+namespace Jypeli.Linux
+{
+	public class JypeliLinuxProject : DotNetProject
+	{
+		public JypeliLinuxProject()
+			: base()
+		{
+		}
+
+		public JypeliLinuxProject(string languageName)
+			: base(languageName)
+		{
+		}
+
+		public JypeliLinuxProject(string languageName, ProjectCreateInformation projectCreateInformation, XmlElement projectOptions)
+			: base(languageName, projectCreateInformation, projectOptions)
+		{
+		}
+
+		protected override BuildResult OnBuild(IProgressMonitor monitor, ConfigurationSelector configuration)
+		{
+			return base.OnBuild(monitor, configuration);
+		}
+	}
+}
+	

c7c1515abd3be4aa021158ebbfa59f2cf1ac780b
Rami Pasanen <10365956+Rampastring@users.noreply.github.com>
2018-01-04 19:49:43 +0000

    get_monogame.bat nyt toimii riippumatta siitä, onko Gitistä asennettuna 32- vai 64-bittinen versio

--- get_monogame.bat
+++ get_monogame.bat
@@ -1,15 +1,22 @@
 @echo off
 setlocal
 
-if exist "C:\Program Files (x86)\Git\bin\sh.exe" goto checkmonogame
+if exist MonoGame\.git goto end
+
+if exist "%ProgramFiles%\Git\bin\sh.exe" goto getmonogame32
+if exist "%ProgramFiles(x86)%\Git\bin\sh.exe" goto getmonogame6432
 echo Git not found
 echo Please go to https://git-scm.com and install the latest version.
 goto error
 
-:checkmonogame
-if exist MonoGame\.git goto end
+:getmonogame32
 pushd MonoGame
-"C:\Program Files (x86)\git\bin\sh.exe" module_init.sh
+"%ProgramFiles%\git\bin\sh.exe" module_init.sh
+popd
+
+:getmonogame6432
+pushd MonoGame
+"%ProgramFiles(x86)%\git\bin\sh.exe" module_init.sh
 popd
 
 :error

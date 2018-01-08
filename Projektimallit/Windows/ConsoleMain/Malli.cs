8c5039a6fe0ce6515391e47d234c120ff406ea1f
Rami Pasanen <10365956+Rampastring@users.noreply.github.com>
2018-01-08 14:35:15 +0000

    Lisätty ConsoleMain -projektimallin tiedostot

---
+++ Projektimallit/Windows/ConsoleMain/Malli.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author $username$
+/// @version ..$year$
+/// <summary>
+/// 
+/// </summary>
+public class $safeprojectname$
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+
+    }
+
+}

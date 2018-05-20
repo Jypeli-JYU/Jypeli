23e36af3e11e4c54da5469ef1114d4c584271e05
Rami Pasanen <10365956+Rampastring@users.noreply.github.com>
2018-05-20 14:49:06 +0000

    Päivitetty MacOS-projektimalleja: poistettu SimplePhysics ja päivitetty kohde-MacOS-versiota uudemmaksi, jotta toimii viimeisimmällä Visual Studio for Macilla

--- Projektimallit/Xamarin-NuGet/Jypeli.MacOS/Properties/AddinInfo.cs
+++ Projektimallit/Xamarin-NuGet/Jypeli.MacOS/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly: Addin (
     "Jypeli",
     Namespace = "Jypeli.MacOS",
-    Version = "6.8.5"
+    Version = "6.8.8"
 )]
 
 [assembly: AddinName ("Jypeli")]

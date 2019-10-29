fe375242f7f792ec6a4da9d088ff5ce371ff5356
Mikko <mikko.roysko@gmail.com>
2019-10-29 23:40:19 +0200

    More functionality to test project

--- testit/Tasohyppelypeli/Norsupeli/Tasohyppelypeli.cs
+++ testit/Tasohyppelypeli/Norsupeli/Tasohyppelypeli.cs
@@ -7,17 +7,20 @@ using Jypeli.Widgets;
 
 public class Tasohyppelypeli : PhysicsGame
 {
-    const double Nopeus = 200;
-    const double HyppyNopeus = 750;
-    const int RUUDUN_KOKO = 40;
+    private const double NOPEUS = 200;
+    private const double HYPPYNOPEUS = 750;
+    private const int RUUDUN_KOKO = 40;
 
-    PlatformCharacter pelaaja1;
+    private PlatformCharacter pelaaja1;
 
-    Image pelaajanKuva = LoadImage("norsu.png");
-    Image tahtiKuva = LoadImage("tahti.png");
+    private Image pelaajanKuva = LoadImage("norsu.png");
+    private Image tahtiKuva = LoadImage("tahti.png");
 
-    SoundEffect maaliAani = LoadSoundEffect("maali.wav");
+    private SoundEffect maaliAani = LoadSoundEffect("maali.wav");
 
+    private int tahtia = 0;
+
+    IntMeter pistelaskuri = new IntMeter(0);
     public override void Begin()
     {
         Gravity = new Vector(0, -1000);
@@ -25,12 +28,26 @@ public class Tasohyppelypeli : PhysicsGame
         LuoKentta();
         LisaaNappaimet();
 
+        pistelaskuri.MaxValue = tahtia;
+        pistelaskuri.UpperLimit += kaikki;
+
         Camera.Follow(pelaaja1);
         Camera.ZoomFactor = 1.2;
         Camera.StayInLevel = true;
     }
 
-    void LuoKentta()
+    private void kaikki()
+    {
+        EasyHighScore e = new EasyHighScore();
+        e.EnterAndShow(tahtia);
+        e.HighScoreWindow.Closed += delegate
+        {
+            ClearAll();
+            Begin();
+        };
+    }
+
+    private void LuoKentta()
     {
         TileMap kentta = TileMap.FromLevelAsset("kentta1.txt");
         kentta.SetTileMethod('#', LisaaTaso);
@@ -41,7 +58,7 @@ public class Tasohyppelypeli : PhysicsGame
         Level.Background.CreateGradient(Color.White, Color.SkyBlue);
     }
 
-    void LisaaTaso(Vector paikka, double leveys, double korkeus)
+    private void LisaaTaso(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
         taso.Position = paikka;
@@ -49,7 +66,7 @@ public class Tasohyppelypeli : PhysicsGame
         Add(taso);
     }
 
-    void LisaaTahti(Vector paikka, double leveys, double korkeus)
+    private void LisaaTahti(Vector paikka, double leveys, double korkeus)
     {
         PhysicsObject tahti = PhysicsObject.CreateStaticObject(leveys, korkeus);
         tahti.IgnoresCollisionResponse = true;
@@ -57,9 +74,11 @@ public class Tasohyppelypeli : PhysicsGame
         tahti.Image = tahtiKuva;
         tahti.Tag = "tahti";
         Add(tahti);
+
+        tahtia++;
     }
 
-    void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
+    private void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
     {
         pelaaja1 = new PlatformCharacter(leveys, korkeus);
         pelaaja1.Position = paikka;
@@ -67,40 +86,66 @@ public class Tasohyppelypeli : PhysicsGame
         pelaaja1.Image = pelaajanKuva;
         AddCollisionHandler(pelaaja1, "tahti", TormaaTahteen);
         Add(pelaaja1);
+
+        AssaultRifle a = new AssaultRifle(50, 10);
+        pelaaja1.Weapon = a;
+
     }
 
-    void LisaaNappaimet()
+    private void LisaaNappaimet()
     {
         Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
 
-        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -Nopeus);
-        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, Nopeus);
-        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HyppyNopeus);
+        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -NOPEUS);
+        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, NOPEUS);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
 
         ControllerOne.Listen(Button.Back, ButtonState.Pressed, Exit, "Poistu pelistä");
 
-        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1, -Nopeus);
-        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, Nopeus);
-        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HyppyNopeus);
+        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1, -NOPEUS);
+        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, NOPEUS);
+        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
 
         PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+
+        Mouse.ListenMovement(0.1, Tahtaa, "Tähtää aseella");
+        Mouse.Listen(MouseButton.Left, ButtonState.Down, AmmuAseella, "ammu", pelaaja1.Weapon);
     }
 
-    void Liikuta(PlatformCharacter hahmo, double nopeus)
+    void Tahtaa(AnalogState hiirenLiike)
+    {
+        Vector suunta = (Mouse.PositionOnWorld - pelaaja1.Weapon.AbsolutePosition).Normalize();
+        pelaaja1.Weapon.Angle = suunta.Angle;
+    }
+
+    void AmmuAseella(Weapon ase)
+    {
+        PhysicsObject ammus = ase.Shoot();
+
+        if (ammus != null)
+        {
+            //ammus.Size *= 3;
+            //ammus.Image = ...
+            //ammus.MaximumLifetime = TimeSpan.FromSeconds(2.0);
+        }
+    }
+
+    private void Liikuta(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Walk(nopeus);
     }
 
-    void Hyppaa(PlatformCharacter hahmo, double nopeus)
+    private void Hyppaa(PlatformCharacter hahmo, double nopeus)
     {
         hahmo.Jump(nopeus);
     }
 
-    void TormaaTahteen(PhysicsObject hahmo, PhysicsObject tahti)
+    private void TormaaTahteen(PhysicsObject hahmo, PhysicsObject tahti)
     {
         maaliAani.Play();
         MessageDisplay.Add("Keräsit tähden!");
         tahti.Destroy();
+        pistelaskuri.Value++;
     }
 }

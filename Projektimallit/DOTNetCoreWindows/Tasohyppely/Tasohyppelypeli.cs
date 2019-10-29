9e81475b5e993fd9f1976ef9ccfcad2c2479dbf7
Mikko Röyskö <mikko.roysko@gmail.com>
2019-10-29 18:24:39 +0200

    Initial project template creation

---
+++ Projektimallit/DOTNetCoreWindows/Tasohyppely/Tasohyppelypeli.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using Jypeli;
+using Jypeli.Assets;
+using Jypeli.Controls;
+using Jypeli.Widgets;
+
+public class Tasohyppelypeli : PhysicsGame
+{
+    private const double NOPEUS = 200;
+    private const double HYPPYNOPEUS = 750;
+    private const int RUUDUN_KOKO = 40;
+
+    private PlatformCharacter pelaaja1;
+
+    private Image pelaajanKuva = LoadImage("norsu.png");
+    private Image tahtiKuva = LoadImage("tahti.png");
+
+    private SoundEffect maaliAani = LoadSoundEffect("maali.wav");
+
+    public override void Begin()
+    {
+        Gravity = new Vector(0, -1000);
+
+        LuoKentta();
+        LisaaNappaimet();
+
+        Camera.Follow(pelaaja1);
+        Camera.ZoomFactor = 1.2;
+        Camera.StayInLevel = true;
+    }
+
+    private void LuoKentta()
+    {
+        TileMap kentta = TileMap.FromLevelAsset("kentta1.txt");
+        kentta.SetTileMethod('#', LisaaTaso);
+        kentta.SetTileMethod('*', LisaaTahti);
+        kentta.SetTileMethod('N', LisaaPelaaja);
+        kentta.Execute(RUUDUN_KOKO, RUUDUN_KOKO);
+        Level.CreateBorders();
+        Level.Background.CreateGradient(Color.White, Color.SkyBlue);
+    }
+
+    private void LisaaTaso(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject taso = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        taso.Position = paikka;
+        taso.Color = Color.Green;
+        Add(taso);
+    }
+
+    private void LisaaTahti(Vector paikka, double leveys, double korkeus)
+    {
+        PhysicsObject tahti = PhysicsObject.CreateStaticObject(leveys, korkeus);
+        tahti.IgnoresCollisionResponse = true;
+        tahti.Position = paikka;
+        tahti.Image = tahtiKuva;
+        tahti.Tag = "tahti";
+        Add(tahti);
+    }
+
+    private void LisaaPelaaja(Vector paikka, double leveys, double korkeus)
+    {
+        pelaaja1 = new PlatformCharacter(leveys, korkeus);
+        pelaaja1.Position = paikka;
+        pelaaja1.Mass = 4.0;
+        pelaaja1.Image = pelaajanKuva;
+        AddCollisionHandler(pelaaja1, "tahti", TormaaTahteen);
+        Add(pelaaja1);
+    }
+
+    private void LisaaNappaimet()
+    {
+        Keyboard.Listen(Key.F1, ButtonState.Pressed, ShowControlHelp, "Näytä ohjeet");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, -NOPEUS);
+        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikkuu vasemmalle", pelaaja1, NOPEUS);
+        Keyboard.Listen(Key.Up, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
+
+        ControllerOne.Listen(Button.Back, ButtonState.Pressed, Exit, "Poistu pelistä");
+
+        ControllerOne.Listen(Button.DPadLeft, ButtonState.Down, Liikuta, "Pelaaja liikkuu vasemmalle", pelaaja1, -NOPEUS);
+        ControllerOne.Listen(Button.DPadRight, ButtonState.Down, Liikuta, "Pelaaja liikkuu oikealle", pelaaja1, NOPEUS);
+        ControllerOne.Listen(Button.A, ButtonState.Pressed, Hyppaa, "Pelaaja hyppää", pelaaja1, HYPPYNOPEUS);
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+    }
+
+    private void Liikuta(PlatformCharacter hahmo, double nopeus)
+    {
+        hahmo.Walk(nopeus);
+    }
+
+    private void Hyppaa(PlatformCharacter hahmo, double nopeus)
+    {
+        hahmo.Jump(nopeus);
+    }
+
+    private void TormaaTahteen(PhysicsObject hahmo, PhysicsObject tahti)
+    {
+        maaliAani.Play();
+        MessageDisplay.Add("Keräsit tähden!");
+        tahti.Destroy();
+    }
+}

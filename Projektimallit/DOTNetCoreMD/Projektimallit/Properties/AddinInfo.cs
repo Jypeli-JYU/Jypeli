56053fa53cc2bf8244089637fe6debec0104bde5
Mikko <mikko.roysko@gmail.com>
2019-11-04 11:14:57 -0800

    Project templates for MonoDevelop

---
+++ Projektimallit/DOTNetCoreMD/Projektimallit/Properties/AddinInfo.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly: Addin(
+    "Jypeli.Templates",
+    Namespace = "",
+    Version = "1.0.0"
+)]
+
+[assembly: AddinName("Jypeli projektimallit")]
+[assembly: AddinCategory("IDE extensions")]
+[assembly: AddinDescription("Jypelin projektimallit")]
+[assembly: AddinAuthor("University of Jyväskylä")]

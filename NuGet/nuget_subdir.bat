a7c629fb255ff346134f839261e4c771e2910d40
Mikko <mikko.roysko@gmail.com>
2020-10-17 19:29:50 +0300

    Repo cleanup

--- NuGet/nuget_subdir.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-REM Usage: call nuget_subdir <subdir name> <nuget command>
-
-setlocal
-pushd %1
-..\nuget.exe %2
-popd
-endlocal
--- NuGet/release.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-
-call clean.bat
-call build.bat
-
-for /R . %%G IN (*.nupkg) do nuget push "%%G" -Source https://www.nuget.org/api/v2/package
-
-endlocal

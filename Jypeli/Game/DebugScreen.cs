ceccc72e3235d3efa6a6fc6089748413e3002abe
Mikko <mikko.roysko@gmail.com>
2021-01-16 21:35:22 +0200

    Drawing options for DebugScreen

--- Jypeli/Game/DebugScreen.cs
+++ Jypeli/Game/DebugScreen.cs
@@ -9,6 +9,51 @@ namespace Jypeli
         private int fpsSkipCounter;
         private Canvas debugCanvas;
         private Matrix canvasTransform = Matrix.Identity;
+        private Camera camera { get => Game.Instance.Camera; }
+
+        #region debugsettings
+        /// <summary>
+        /// Debug-näkymän (F12) piirtoasetuksia
+        /// </summary>
+        public struct DebugViewSettings
+        {
+            /// <summary>
+            /// Piirretäänkö objektien ääriviivoja
+            /// </summary>
+            public static bool DrawOutlines = true;
+
+            /// <summary>
+            /// Piirretäänkö fysiikkamuotojen verteksien ääriviivat.
+            /// Toimii ainoastaan Farseer-moottorilla.
+            /// </summary>
+            public static bool DrawPhysicsOutlines = true;
+
+            /// <summary>
+            /// Millä värillä <c>GameObject</c>ien ääriviivat piirretään
+            /// </summary>
+            public static Color GameObjectColor = Color.LightGray;
+
+            /// <summary>
+            /// Millä värillä <c>GameObject</c>ien ääriviivat piirretään kun hiiri on niiden päällä
+            /// </summary>
+            public static Color GameObjectHoverColor = Color.LightGreen;
+
+            /// <summary>
+            /// Millä värillä <c>PhysicsObject</c>ien ääriviivat piirretään
+            /// </summary>
+            public static Color PhysicsObjectColor = Color.DarkRed;
+
+            /// <summary>
+            /// Millä värillä <c>PhysicsObject</c>ien ääriviivat piirretään kun hiiri on niiden päällä
+            /// </summary>
+            public static Color PhysicsObjectHoverColor = Color.Salmon;
+
+            /// <summary>
+            /// Millä värillä <c>PhysicsObject</c>ien fysiikkamuotojen verteksien ääriviivat piirretään
+            /// </summary>
+            public static Color PhysicsObjectVertexColor = Color.Gray;
+        }
+        #endregion
 
         /// <summary>
         /// Debug-ruutukerros, joka näkyy kun painetaan F12.
@@ -46,8 +91,6 @@ namespace Jypeli
         /// </summary>
         public Label LayerDisplay { get; private set; }
 
-        private Camera camera { get => Game.Instance.Camera; }
-
         private void InitDebugScreen()
         {
             debugCanvas = new Canvas();
@@ -59,7 +102,7 @@ namespace Jypeli
             FPSWindow = new Jypeli.Window();
             FPSWindow.IsModal = false;
             FPSWindow.Color = new Color( Color.HotPink, 100 );
-            FPSWindow.Right = Screen.Right - Screen.Width / 16;
+            FPSWindow.Right = Screen.Right - Screen.Width / 16; // TODO: Näiden sijaintien pitäisi muuttua ikkunan koon mukana.
             FPSWindow.Top = Screen.Top;
             DebugLayer.Add( FPSWindow );
 
@@ -138,19 +181,20 @@ namespace Jypeli
                 return;
 
             debugCanvas.Begin( ref canvasTransform, Game.Screen );
-            PaintDebugScreen( debugCanvas );
+            if(DebugViewSettings.DrawOutlines)
+                PaintDebugScreen( debugCanvas );
             debugCanvas.End();
             
             DebugLayer.Draw( Camera );
         }
 
-        private void PaintShapeOutlines( Canvas canvas, IGameObject obj )
+        private void PaintShapeOutlines(Canvas canvas, IGameObject obj, Color color)
         {
             var vertexes = obj.Shape.Cache.OutlineVertices;
             double wmul = obj.Shape.IsUnitSize ? obj.Width : 1;
             double hmul = obj.Shape.IsUnitSize ? obj.Height : 1;
 
-            canvas.BrushColor = ( obj is GameObject && Mouse.IsCursorOn( (GameObject)obj ) ) ? Color.LightGreen : Color.LightGray;
+            canvas.BrushColor = color;
 
             Matrix transform =
                      Matrix.CreateRotationZ((float)obj.Angle.Radians)
@@ -185,78 +229,55 @@ namespace Jypeli
             }
         }
 
-        private void PaintPhysicsOutlines( Canvas canvas, PhysicsObject obj )
+        private void PaintPhysicsOutlines(Canvas canvas, PhysicsObject obj, Color color)
         {
             if ( obj.Body == null || obj.Body.Shape == null || obj.Body.Shape.Cache == null )
                 return;
 
-            var vertexes = obj.Body.Shape.Cache.OutlineVertices;
-            double wmul = obj.Body.Shape.IsUnitSize ? obj.Width : 1;
-            double hmul = obj.Body.Shape.IsUnitSize ? obj.Height : 1;
+            var vertices = obj.Body.Vertices;
+            double wmul = 100; // Farseerin SimToDisplay-kerroin. Tämä pitäiis varmaan yhdistää jotenkin siihen.
+            double hmul = 100;
 
-            canvas.BrushColor = ( obj is GameObject && Mouse.IsCursorOn( (GameObject)obj ) ) ? Color.Salmon : Color.DarkRed;
+            canvas.BrushColor = color;
 
-            Vector3 center = (Vector3)Camera.WorldToScreen( obj.Body.Position, obj.Layer );
-            Matrix transform = Matrix.CreateRotationZ( (float)obj.Body.Angle ) * Matrix.CreateTranslation( center );
+            Matrix transform =
+                     Matrix.CreateRotationZ((float)obj.Angle.Radians)
+                    * Matrix.CreateTranslation((float)obj.Position.X, (float)obj.Position.Y, 0f)
+                    * Matrix.CreateTranslation(-(float)camera.Position.X, -(float)camera.Position.Y, 0f)
+                    * Matrix.CreateScale((float)(camera.ZoomFactor), (float)(camera.ZoomFactor), 1f);
 
-            for ( int j = 0; j < vertexes.Length - 1; j++ )
+            foreach (var vertexes in vertices)
             {
-                double x1 = wmul * vertexes[j].X;
-                double y1 = hmul * vertexes[j].Y;
-                double x2 = wmul * vertexes[j + 1].X;
-                double y2 = hmul * vertexes[j + 1].Y;
+                for (int j = 0; j < vertexes.Count - 1; j++)
+                {
+                    double x1 = wmul * vertexes[j].X;
+                    double y1 = hmul * vertexes[j].Y;
+                    double x2 = wmul * vertexes[j + 1].X;
+                    double y2 = hmul * vertexes[j + 1].Y;
 
-                var t1 = Vector2.Transform( new Vector2( (float)x1, (float)y1 ), transform );
-                var t2 = Vector2.Transform( new Vector2( (float)x2, (float)y2 ), transform );
+                    var t1 = Vector2.Transform(new Vector2((float)x1, (float)y1), transform);
+                    var t2 = Vector2.Transform(new Vector2((float)x2, (float)y2), transform);
 
-                canvas.DrawLine( t1.X, t1.Y, t2.X, t2.Y );
+                    canvas.DrawLine(t1.X, t1.Y, t2.X, t2.Y);
+                }
+
+                if (vertexes.Count > 2)
+                {
+                    double x1 = wmul * vertexes[vertexes.Count - 1].X;
+                    double y1 = hmul * vertexes[vertexes.Count - 1].Y;
+                    double x2 = wmul * vertexes[0].X;
+                    double y2 = hmul * vertexes[0].Y;
+
+                    var t1 = Vector2.Transform(new Vector2((float)x1, (float)y1), transform);
+                    var t2 = Vector2.Transform(new Vector2((float)x2, (float)y2), transform);
+
+                    canvas.DrawLine(t1.X, t1.Y, t2.X, t2.Y);
+                }
             }
-
-            if ( vertexes.Length > 2 )
-            {
-                double x1 = wmul * vertexes[vertexes.Length - 1].X;
-                double y1 = hmul * vertexes[vertexes.Length - 1].Y;
-                double x2 = wmul * vertexes[0].X;
-                double y2 = hmul * vertexes[0].Y;
-
-                var t1 = Vector2.Transform( new Vector2( (float)x1, (float)y1 ), transform );
-                var t2 = Vector2.Transform( new Vector2( (float)x2, (float)y2 ), transform );
-
-                canvas.DrawLine( t1.X, t1.Y, t2.X, t2.Y );
-            }
-
-            /*var vertexes = obj.Body.Shape.Cache.OutlineVertices;
-            var center = Camera.WorldToScreen( obj.Body.Position, obj.Layer );
-            double wmul = obj.Body.Shape.IsUnitSize ? obj.Width : 1;
-            double hmul = obj.Body.Shape.IsUnitSize ? obj.Height : 1;
-
-            canvas.BrushColor = ( obj is GameObject && Mouse.IsCursorOn( (GameObject)obj ) ) ? Color.Salmon : Color.DarkRed;
-
-            for ( int j = 0; j < vertexes.Length - 1; j++ )
-            {
-                double x1 = center.X + wmul * vertexes[j].X;
-                double y1 = center.Y + hmul * vertexes[j].Y;
-                double x2 = center.X + wmul * vertexes[j + 1].X;
-                double y2 = center.Y + hmul * vertexes[j + 1].Y;
-
-                canvas.DrawLine( x1, y1, x2, y2 );
-            }
-
-            if ( vertexes.Length > 2 )
-            {
-                double x1 = center.X + wmul * vertexes[vertexes.Length - 1].X;
-                double y1 = center.Y + hmul * vertexes[vertexes.Length - 1].Y;
-                double x2 = center.X + wmul * vertexes[0].X;
-                double y2 = center.Y + hmul * vertexes[0].Y;
-
-                canvas.DrawLine( x1, y1, x2, y2 );
-            }*/
         }
 
         private void PaintDebugScreen( Canvas canvas )
         {
-            // Draw the object outlines as determined by their shape
-
             for ( int i = Layers.FirstIndex; i <= Layers.LastIndex; i++ )
             {
                 foreach ( var obj in Layers[i].Objects )
@@ -264,10 +285,15 @@ namespace Jypeli
                     if ( obj == null || obj.Shape == null || obj.Shape.Cache == null || obj.Layer == null || obj is Widget) // Toistaiseksi ei piirretä widgettien ääriviivoja (halutaanko edes?)
                         continue;
 
-                    PaintShapeOutlines( canvas, obj );
+                    if (obj is PhysicsObject)
+                    {
+                        PaintShapeOutlines(canvas, obj, Mouse.IsCursorOn((GameObject)obj) ? DebugViewSettings.PhysicsObjectHoverColor : DebugViewSettings.PhysicsObjectColor);
+                        if(DebugViewSettings.DrawPhysicsOutlines)
+                            PaintPhysicsOutlines(canvas, (PhysicsObject)obj, DebugViewSettings.PhysicsObjectVertexColor);
+                    }
+                    else
+                        PaintShapeOutlines(canvas, obj, Mouse.IsCursorOn((GameObject)obj) ? DebugViewSettings.GameObjectHoverColor : DebugViewSettings.GameObjectColor);
 
-                    //if ( obj is PhysicsObject ) // TODO: fysiikkamuotojen sisäiset verteksit
-                    //    PaintPhysicsOutlines( canvas, (PhysicsObject)obj );
                 }
             }
         }

6e93f3bb91289a7da03ba870119c80c46184bd18
Mikko <mikko.roysko@gmail.com>
2021-07-08 14:47:46 +0300

    Fix DebugScreen window positions

--- Jypeli/Game/DebugScreen.cs
+++ Jypeli/Game/DebugScreen.cs
@@ -104,30 +104,26 @@ namespace Jypeli
             DebugLayer.Objects.ItemAdded += OnObjectAdded;
             DebugLayer.Objects.ItemRemoved += OnObjectRemoved;
 
-            FPSWindow = new Jypeli.Window();
+            FPSWindow = new Window();
             FPSWindow.IsModal = false;
-            FPSWindow.Color = new Color( Color.HotPink, 100 );
-            FPSWindow.Right = Screen.Right - Screen.Width / 16; // TODO: Näiden sijaintien pitäisi muuttua ikkunan koon mukana.
-            FPSWindow.Top = Screen.Top;
-            DebugLayer.Add( FPSWindow );
+            FPSWindow.Color = new Color(Color.HotPink, 100);
+            DebugLayer.Add(FPSWindow);
 
-            FPSDisplay = new Label( "00" );
+            FPSDisplay = new Label("00");
             FPSDisplay.Color = Color.HotPink;
             FPSDisplay.Position = FPSWindow.Position;
             FPSWindow.Size = 1.5 * FPSDisplay.Size;
-            FPSWindow.Add( FPSDisplay );
+            FPSWindow.Add(FPSDisplay);
 
-            LayerWindow = new Jypeli.Window();
+            LayerWindow = new Window();
             LayerWindow.IsModal = false;
-            LayerWindow.Color = new Color( Color.Blue, 100 );
-            LayerWindow.Left = Screen.Left;
-            LayerWindow.Top = Screen.Top - 200;
-            DebugLayer.Add( LayerWindow );
+            LayerWindow.Color = new Color(Color.Blue, 100);
+            DebugLayer.Add(LayerWindow);
 
-            LayerDisplay = new Label( "Layers: no data" );
+            LayerDisplay = new Label("Layers: no data");
             LayerDisplay.TextColor = Color.White;
             LayerDisplay.Position = LayerWindow.Position;
-            LayerWindow.Add( LayerDisplay );
+            LayerWindow.Add(LayerDisplay);
             LayerWindow.Size = LayerDisplay.Size;
 
 
@@ -152,27 +148,43 @@ namespace Jypeli
             }
         }
 
-        private void UpdateDebugScreen( Time time )
+        private void UpdateDebugScreen(Time time)
         {
-            if ( DebugKeyEnabled && Keyboard.GetKeyState( Key.F12 ) == ButtonState.Pressed )
+            if (DebugKeyEnabled && Keyboard.GetKeyState(Key.F12) == ButtonState.Pressed)
+            {
                 DebugScreenVisible = !DebugScreenVisible;
 
-            DebugLayer.Update( time );
+                // Päivitetään elementtien sijainti ruudulle oikeaan kohtaan
+                UpdateLayerWindow(); // Ensimmäistä aukaisua varten pitää täydentää elementtien sisällöt
+                FPSDisplay.Text = fpsText;
 
-            if ( !DebugScreenVisible )
+                LayerWindow.Left = Screen.Left;
+                LayerWindow.Top = Screen.Top;
+
+                FPSWindow.Right = Screen.Right;
+                FPSWindow.Top = Screen.Top;
+            }
+
+            DebugLayer.Update(time);
+
+            if (!DebugScreenVisible)
                 return;
 
             FPSDisplay.Text = fpsText;
+            UpdateLayerWindow();
+        }
 
+        private void UpdateLayerWindow()
+        {
             layerTextBuilder.Clear();
-            layerTextBuilder.Append( layerTextTitle );
+            layerTextBuilder.Append(layerTextTitle);
 
             for (int i = Layers.FirstIndex; i <= Layers.LastIndex; i++)
             {
-                layerTextBuilder.Append( "[" );
-                layerTextBuilder.Append( i );
-                layerTextBuilder.Append( "]: " );
-                layerTextBuilder.Append( Layers[i].Objects.Count );
+                layerTextBuilder.Append("[");
+                layerTextBuilder.Append(i);
+                layerTextBuilder.Append("]: ");
+                layerTextBuilder.Append(Layers[i].Objects.Count);
                 layerTextBuilder.AppendLine();
             }
 
@@ -182,15 +194,15 @@ namespace Jypeli
 
         private void DrawDebugScreen()
         {
-            if ( !DebugScreenVisible )
+            if (!DebugScreenVisible)
                 return;
 
-            debugCanvas.Begin( ref canvasTransform, Game.Screen );
-            if(DebugViewSettings.DrawOutlines)
-                PaintDebugScreen( debugCanvas );
+            debugCanvas.Begin(ref canvasTransform, Screen);
+            if (DebugViewSettings.DrawOutlines)
+                PaintDebugScreen(debugCanvas);
             debugCanvas.End();
-            
-            DebugLayer.Draw( Camera );
+
+            DebugLayer.Draw(Camera);
         }
 
         private void PaintShapeOutlines(Canvas canvas, IGameObject obj, Color color)
@@ -207,36 +219,36 @@ namespace Jypeli
                     * Matrix.CreateTranslation(-(float)camera.Position.X, -(float)camera.Position.Y, 0f)
                     * Matrix.CreateScale((float)(camera.ZoomFactor), (float)(camera.ZoomFactor), 1f);
 
-            for ( int j = 0; j < vertexes.Length - 1; j++ )
+            for (int j = 0; j < vertexes.Length - 1; j++)
             {
                 double x1 = wmul * vertexes[j].X;
                 double y1 = hmul * vertexes[j].Y;
                 double x2 = wmul * vertexes[j + 1].X;
                 double y2 = hmul * vertexes[j + 1].Y;
 
-                var t1 = Vector2.Transform( new Vector2( (float)x1, (float)y1 ), transform );
-                var t2 = Vector2.Transform( new Vector2( (float)x2, (float)y2 ), transform );
+                var t1 = Vector2.Transform(new Vector2((float)x1, (float)y1), transform);
+                var t2 = Vector2.Transform(new Vector2((float)x2, (float)y2), transform);
 
-                canvas.DrawLine( t1.X, t1.Y, t2.X, t2.Y );
+                canvas.DrawLine(t1.X, t1.Y, t2.X, t2.Y);
             }
 
-            if ( vertexes.Length > 2 )
+            if (vertexes.Length > 2)
             {
                 double x1 = wmul * vertexes[vertexes.Length - 1].X;
                 double y1 = hmul * vertexes[vertexes.Length - 1].Y;
                 double x2 = wmul * vertexes[0].X;
                 double y2 = hmul * vertexes[0].Y;
-            
-                var t1 = Vector2.Transform( new Vector2( (float)x1, (float)y1 ), transform );
-                var t2 = Vector2.Transform( new Vector2( (float)x2, (float)y2 ), transform );
-            
-                canvas.DrawLine( t1.X, t1.Y, t2.X, t2.Y );
+
+                var t1 = Vector2.Transform(new Vector2((float)x1, (float)y1), transform);
+                var t2 = Vector2.Transform(new Vector2((float)x2, (float)y2), transform);
+
+                canvas.DrawLine(t1.X, t1.Y, t2.X, t2.Y);
             }
-            if(obj.Shape == Shape.Circle && DebugViewSettings.DrawCircleRotation)
+            if (obj.Shape == Shape.Circle && DebugViewSettings.DrawCircleRotation)
             {
                 double x1 = 0;
                 double y1 = 0;
-                double x2 = obj.Width/2;
+                double x2 = obj.Width / 2;
                 double y2 = 0;
 
                 var t1 = Vector2.Transform(new Vector2((float)x1, (float)y1), transform);
@@ -248,7 +260,7 @@ namespace Jypeli
 
         private void PaintPhysicsOutlines(Canvas canvas, PhysicsObject obj, Color color)
         {
-            if ( obj.Body == null || obj.Body.Shape == null || obj.Body.Shape.Cache == null )
+            if (obj.Body == null || obj.Body.Shape == null || obj.Body.Shape.Cache == null)
                 return;
 
             var vertices = obj.Body.Vertices;
@@ -293,35 +305,34 @@ namespace Jypeli
             }
         }
 
-        private void PaintDebugScreen( Canvas canvas )
+        private void PaintDebugScreen(Canvas canvas)
         {
-            for ( int i = Layers.FirstIndex; i <= Layers.LastIndex; i++ )
+            for (int i = Layers.FirstIndex; i <= Layers.LastIndex; i++)
             {
-                foreach ( var obj in Layers[i].Objects )
+                foreach (var obj in Layers[i].Objects)
                 {
-                    if ( obj == null || obj.Shape == null || obj.Shape.Cache == null || obj.Layer == null || obj is Widget) // Toistaiseksi ei piirretä widgettien ääriviivoja (halutaanko edes?)
+                    if (obj == null || obj.Shape == null || obj.Shape.Cache == null || obj.Layer == null || obj is Widget) // Toistaiseksi ei piirretä widgettien ääriviivoja (halutaanko edes?)
                         continue;
 
                     if (obj is PhysicsObject)
                     {
                         PaintShapeOutlines(canvas, obj, Mouse.IsCursorOn((GameObject)obj) ? DebugViewSettings.PhysicsObjectHoverColor : DebugViewSettings.PhysicsObjectColor);
-                        if(DebugViewSettings.DrawPhysicsOutlines)
+                        if (DebugViewSettings.DrawPhysicsOutlines)
                             PaintPhysicsOutlines(canvas, (PhysicsObject)obj, DebugViewSettings.PhysicsObjectVertexColor);
                     }
                     else
                     {
-                        if(obj is PhysicsStructure)
+                        if (obj is PhysicsStructure)
                         {
                             PhysicsStructure ps = obj as PhysicsStructure;
                             foreach (var o in ps.Objects)
                             {
                                 PaintShapeOutlines(canvas, o, Mouse.IsCursorOn(o) ? DebugViewSettings.GameObjectHoverColor : DebugViewSettings.GameObjectColor);
-                            } 
-                        }else
+                            }
+                        }
+                        else
                             PaintShapeOutlines(canvas, obj, Mouse.IsCursorOn((GameObject)obj) ? DebugViewSettings.GameObjectHoverColor : DebugViewSettings.GameObjectColor);
                     }
-                        
-
                 }
             }
         }

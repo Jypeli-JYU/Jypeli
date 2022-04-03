0c5d783c5624d06315904346acc37f7cfcd52bef
Mikko <mikko.roysko@gmail.com>
2022-04-03 12:27:09 +0300

    Create JypeliActivity for Android.

---
+++ Jypeli/Game/JypeliActivity.cs
@@ -0,0 +1,23 @@
+﻿#if ANDROID
+using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Silk.NET.Windowing.Sdl.Android;
+
+namespace Jypeli
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
+    public class JypeliActivity : SilkActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            ActionBar.Hide();
+            Game.AssetManager = Assets;
+            base.OnCreate(savedInstanceState);
+        }
+        protected override void OnRun()
+        {
+        }
+    }
+}
+#endif
--- Tests/Net6Android/MainActivity.cs
+++ Tests/Net6Android/MainActivity.cs
@@ -1,25 +1,15 @@
 using Android.App;
-using Android.Content.PM;
-using Android.OS;
-using Android.Views;
-using Silk.NET.Windowing;
-using Silk.NET.Windowing.Sdl.Android;
+using Jypeli;
 
 namespace Net6Android
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
-    public class MainActivity : SilkActivity
+    [Activity(MainLauncher = true)]    
+    public class MainActivity : JypeliActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            ActionBar.Hide();
-            base.OnCreate(savedInstanceState);
-        }
         protected override void OnRun()
         {
-            Jypeli.Game.AssetManager = Assets;
-            using (var game = new Tasohyppelypeli())
-                game.Run();
+            using var game = new Tasohyppelypeli();
+            game.Run();
         }
     }
 }
